6a084ee22ec31a6fe46bf4d001acd5ab7478cb21
brunowang <625653507@qq.com>
2025-09-02 23:40:40 +0800

    实现更通用的json转换，而不是固定的字段

--- src/process.rs
+++ src/process.rs
@@ -1,5 +1,6 @@
 use csv::Reader;
 use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
 use std::fs;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -17,9 +18,16 @@ struct Player {
 pub fn process_csv(input: &str, output: &str) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input)?;
     let mut players = Vec::with_capacity(128);
-    for res in reader.deserialize() {
-        let player: Player = res?;
-        players.push(player);
+    let header = reader.headers()?.clone();
+    for res in reader.records() {
+        let record = res?;
+        let mut player = HashMap::new();
+        for (i, value) in record.iter().enumerate() {
+            let name = &header[i];
+            let value = value.to_string();
+            player.insert(name, value);
+        }
+        players.push(serde_json::json!(player));
     }
     let json = serde_json::to_string_pretty(&players)?;
     fs::write(output, json)?;
